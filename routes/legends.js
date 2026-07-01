9671be3af86ccc347c1a078d7bcc8a05b65232cf
John curran Davis <johncurrandavis@gmail.com>
2026-07-01 09:28:11 +0100

    more on route modules

--- routes/legends.js
+++ routes/legends.js
@@ -6,38 +6,38 @@ import { legends } from "../data/legends.js"
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.json(legends);
-});
+// GET  /api/legends  return all legends
+router.get("/", (_, res) => { res.json(legends); });
 
-
+// GET  /api/legends/:id  return a single legend by id
 router.get("/:id", (req, res) => {
-  const legend = legends.find(
-    l => l.id === Number(req.params.id)
-  );
-
-  if (!legend) {
-    return res.status(404).json({
-      message: "Legend not found"
-    });
-  }
-
+  const legend = legends.find(lgnd => lgnd.id === Number(req.params.id));
+  if (!legend) { return res.status(404).json({ message: "Legend not found." }); }
   res.json(legend);
 });
 
-
+// POST  /api/legends  create a new legend
 router.post("/", (req, res) => {
-
-  const legend = {
-    id: legends.length + 1,
-    ...req.body
-  };
-
+  const legend = { id: legends.length + 1, ...req.body };
   legends.push(legend);
-
   res.status(201).json(legend);
 });
 
+// PUT  /api/legends/:id  update a legend by id
+router.put("/api/legends/:id", (req, res) => {
+  const legend = legends.find(lgnd => lgnd.id === Number(req.params.id));
+  if (!legend) { return res.status(404).json({ message: "Legend not found." }); }
+  legend.name = req.body.name;
+  res.json(legend);
+});
+
+// DELETE  /api/legends/:id  delete a legend by id
+router.delete("/api/legends/:id", (req, res) => {
+  const index = legends.findIndex(lgnd => lgnd.id === Number(req.params.id));
+  if (index === -1) { return res.status(404).json({ message: "Legend not found." }); }
+  legends.splice(index, 1);
+  res.json({ success: true });
+});
 
 export default router;
 
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import legendsRouter from "./routes/legends.js";
 
 
 const app = express();
-const PORT = process.env.PORT || 3000;  // important because Render assigns the port in production
+const PORT = process.env.PORT || 3000;  // Render assigns the port in production
                                         // if process.env.PORT is undefined (running locally) then it defaults to 3000
                                         // Render sets process.env.PORT to a specific number where they expect your web service to be running
 
@@ -20,6 +20,8 @@ app.use(cors());  // allow all origins
 //app.use(cors({ origin: 'https://yourdomain.com' }));
 
 app.use("/api/legends", legendsRouter);
+
+app.get("/", (_, res) => { res.send("Folklore API is running"); });
 
 
 app.listen(PORT);
